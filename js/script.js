6af201f59137ba7a240f13abb733b1a1e30b6769
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-04-18 14:40:23 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
         
-        function hideTabContent(a) {
-            for (let i = a; i < tabContent.length; i++) {
-                tabContent[i].classList.remove('show');
-                tabContent[i].classList.add('hide');
-            }
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
         }
+    }
 
     hideTabContent(1);
 
@@ -33,4 +33,63 @@ window.addEventListener('DOMContentLoaded', ()=>{
             }
         }
     });
+
+    // Timer
+    
+    let deadLine = '2020-05-01';
+
+    function getTimeRemaining(endTime){
+
+        let t = Date.parse(deadLine) - Date.parse(new Date());
+
+        let seconds = Math.floor((t/1000)%60);
+        let minutes = Math.floor((t/1000/60)%60);
+        let hours = Math.floor((t/1000/60/60)%24);
+        let days = Math.floor(t/1000/60/60/24);
+        return{
+            'total' : t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours,
+            'days' : days
+        };
+
+    }
+
+    function setClock(id, endTime){
+        let timer = document.getElementById(id);
+        let days = timer.querySelector('.days');
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock(){
+            let t = getTimeRemaining(endTime);
+
+            function addZero(num){
+                if(num<=9){
+                    return '0'+num;
+                }else{
+                    return num;
+                }
+            }
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if(t.total<=0){
+                clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadLine);
 });
