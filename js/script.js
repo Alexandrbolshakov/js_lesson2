6d10ca008b8b70bbaae00e35149fbd107bf55ae8
Alexandrbolshakov <alexandrbolshakov@yahoo.com>
2020-04-19 08:51:55 +0300

    add div

--- js/script.js
+++ js/script.js
@@ -111,5 +111,25 @@ window.addEventListener('DOMContentLoaded', ()=>{
         document.body.style.overflow = '';
     });
 
-    
+    class Options {
+        constructor(height, width, bg, fontSize, textAlign){
+            this.height = height; 
+            this.width = width;
+            this.bg = bg; 
+            this.fontSize = fontSize; 
+            this.textAlign = textAlign;
+        }
+
+        createDiv(){
+            let elem = document.createElement('div');
+            document.body.appendChild(elem);
+            let param = `height:${this.height}px; width:${this.width}px; background:${this.bg} font-size:${this.fontSize} text-Align:${this.textAlign}`;
+            elem.style.cssText = param;
+        }
+    }
+
+    let div = new Options(300, 350, "red", 14, "center");
+
+
+
 });
